c52178064a0ce998cc73d23219b0bd6562632a8e
Ali Sao <asao1@uci.edu>
2025-02-02 21:55:20 -0800

    create next app, started hero component and completed 1st part of header component. download svg files.

---
+++ animevault/components/Hero.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+function Hero(){
+    return (
+        <header className="bg-hero bg-center bg-cover bg-repeat sm:p-16 px-8 py-16 flex justify-center lg:items-center max-lg: flex-col w-full sm:gap 16">Hello World</header>
+    )
+}
+
+export default Hero;
+
