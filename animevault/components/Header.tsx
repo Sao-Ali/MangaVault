a4484ddfd85971ccb787f25fb41487e68177333f
Ali Sao <asao1@uci.edu>
2025-02-03 22:04:56 -0800

    added a navbar to the application just placeholder at the momment. Need to figure out how to move the logo to the left and everything else to the right. maybe seperate the logo and the header to different div. need to figure out css class. Add login and sign up button. Find a logo

--- animevault/components/Header.tsx
+++ animevault/components/Header.tsx
@@ -1,6 +1,13 @@
+import Image from 'next/image'
+
 function Headers(){
     return (
-        <div className="header">HEADER</div>
+        <div className="sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col sm:gap-16 gap-0">
+            <Image src="/logo.svg" alt="logo" width={40} height={40}/>
+                <h1>Home</h1>
+                <h1>Bookmark</h1>
+                <h1>Comics</h1>
+        </div>
     )
 }
 
