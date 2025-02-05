4764e688bd446863080c6170a5684e6d3360ff4f
Ali Sao <asao1@uci.edu>
2025-02-04 18:56:39 -0800

    add footer

--- animevault/components/Footer.tsx
+++ animevault/components/Footer.tsx
@@ -2,8 +2,33 @@ import Image from "next/image";
 
 function Footer() {
     return (
-        <h1> Hello World!</h1>
-    )
+        <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+            <p className="text-base font-bold text-white">@2023 EpicMangaVault</p>
+            <div className="flex items-center gap-6">
+                <Image
+                    src="./tiktok.svg"
+                    alt="logo"
+                    width={19}
+                    height={19}
+                    className="object-contain"
+                />
+                <Image
+                    src="./instagram.svg"
+                    alt="logo"
+                    width={19}
+                    height={19}
+                    className="object-contain"
+                />
+                <Image
+                    src="./twitter.svg"
+                    alt="logo"
+                    width={19}
+                    height={19}
+                    className="object-contain"
+                />
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
