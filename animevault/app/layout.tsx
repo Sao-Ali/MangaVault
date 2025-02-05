043d652d5c064498b844498320d52176bbbe096b
Ali Sao <asao1@uci.edu>
2025-02-04 18:01:13 -0800

    finished up header bar. decided on a floating navbar instead since when reading manga i dont want it to bother readers. created lib with utils file. currently have icons in for the navbar since can you see on mobile. maybe icon on mobile and text on bigger screen. maybe no icon will have to see.

--- animevault/app/layout.tsx
+++ animevault/app/layout.tsx
@@ -3,8 +3,10 @@ import Hero from "@/components/Hero";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import Headers from "@/components/Header";
+import {FloatingNav} from "@/components/ui/floating-navbar";
 const dmSans = DM_Sans({ subsets: ["latin"] });
+import {FaBook, FaHome} from "react-icons/fa";
+import { FaBookmark} from "react-icons/fa";
 
 
 export const metadata: Metadata = {
@@ -19,12 +21,15 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={dmSans.className}>
-        <Headers />
         <main className="max-w-7xl mx-auto bg-[#0F1117]">
+            <FloatingNav navItems={[
+                {name: 'Home', link:'/', icon: <FaHome />},
+                {name: 'Comics', link:'/', icon:<FaBook />},
+                {name: 'Bookmark', link:'/', icon:<FaBookmark />},
+            ]}/>
             <Hero />
             {children}
         </main>
-
         <Footer />
         </body>
         </html>
